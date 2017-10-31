09a1a60f47c24a4b0c574daae326823bc1496a3f
Vlad Zhukov <work4vlad@gmail.com>
2017-10-31 17:49:11 +0300

    Upgrade deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import {list as babelHelpersList} from 'babel-helpers'; // eslint-disable-line
 import pkg from './package.json';
+
+const externalHelpersWhitelist = babelHelpersList.filter(helperName => helperName !== 'asyncGenerator');
 
 export default {
     input: './src/index.js',
-    plugins: [babel()],
+    plugins: [babel({externalHelpersWhitelist})],
     external: ['@webpack-blocks/webpack'],
     sourcemap: true,
     output: [
